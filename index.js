330f7949496b1db5a1e92455c0c6d6cd9c876312
Brendon Carrasquillo <brendon.wanderlust@gmail.com>
2018-04-19 20:31:28 -0400

    Added AJAX to call omdb api

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 $(function () { 
-    var renderMovieCards = function () {
+    var renderMovieCards = function (arr) {
         $('.card-deck').empty();
-        movieData.forEach(function (thisMovie) {
+        arr.forEach(function (thisMovie) {
             $(".card-deck").append('<div class="card border-dark mt-3 mb-3" style="max-width: 33%; min-width: 20%;"><img class= "card-img-top poster" style = "width:100%;" src = "' + thisMovie.Poster + '" ><div class="card-body"><h5 class="card-title" style="font-weight:bold;"><span>' + thisMovie.Title + ' </span><span>(' + thisMovie.Year + ')</span></h5><button type="button" class="btn btn-default btn-sm add-button" data-id="' + thisMovie.imdbID +'"><i class="far fa-thumbs-up"></i> Add</button></div></div >');
         });
     };
 
     $('form').on('submit', function (e) { 
         e.preventDefault();
-        renderMovieCards();
+        var searchString = $('.search-bar').val();
+        var urlEncodedSearchString = encodeURIComponent(searchString);
+        $.ajax({
+            method: "GET",
+            url: "http://www.omdbapi.com/?apikey=3430a78&s=" + urlEncodedSearchString,
+            success: function(response) {
+                var searchArray = response.Search;
+                movieData = response.Search;
+                renderMovieCards(searchArray);
+            },
+        });    
     });
 
     $('.movies-container').on('click','.add-button', function(){
@@ -24,6 +34,5 @@ $(function () {
         watchlist.push(movie);
         watchlistJSON = JSON.stringify(watchlist);
         localStorage.setItem('watchlist', watchlistJSON);
-    })
-
+    });
 });
